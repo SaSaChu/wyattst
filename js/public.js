bdfadc4f7fe3289d1cf0ae87f8cc63e642a1ad43
SaSaChu <teresa7397@gmail.com>
2017-07-28 23:58:32 +0800

    手機版：menu，banner。pc版：右下

--- js/public.js
+++ js/public.js
@@ -3,6 +3,53 @@
  * @copyright   Copyright (c) 2016 ZEUS Design
  */
 
-$(function () {
+$(function() {
+
+  var $mb = $('#moble_nav_boxs');
+
+  $('.moble_open_icon').click(function() {
+    $mb.addClass('s_open');
+  });
+
+  $('.moble_close_icon').click(function() {
+    $mb.removeClass('s_open');
+  });
+
+
+  $(".item").imgLiquid ();
+
+
+  $('.banner').each (function () {
+
+    $(this).find ('.left').click (function() {
+      // clone是複製前面這組class的意思
+      var a = $(this).find ('.box .item').first ().clone();
+      $(this).find ('.box .item').first ().remove ();
+       // a.appendTo('#box');
+       $(this).find ('.box').append(a);
+       // append是“加入到＿＿前面”
+    }.bind ($(this)));
+
+    $(this).find ('.right').click (function() {
+      var a = $(this).find ('.box .item').last ().clone();
+      $(this).find ('.box .item').last ().remove ();
+      // a.prependTo('#box');
+      $(this).find ('.box').prepend(a);
+      // prepend是“加入到＿＿後面”
+    }.bind ($(this))).click ();
+
+
+    if ($(this).data ('time')) {
+      setInterval (function () {
+        $(this).find ('.left').click ();
+      }.bind ($(this)), parseInt ($(this).data ('time'), 10) * 1000);
+    }
+
+    setTimeout (function () {
+      $(this).addClass ('tran');
+    }.bind ($(this)), 300);
+
+  });
+
 
 });
