0cd4069ea931299852267227f9ff93612340e612
SaSaChu <teresa7397@gmail.com>
2017-08-02 00:06:02 +0800

    add top btn

--- js/public.js
+++ js/public.js
@@ -18,6 +18,7 @@ $(function() {
 
   $(".banner .item").imgLiquid ();
 
+
   $('.banner').each (function () {
     var $that = $(this), $a = $that.find ('a'), $points = $that.find ('.points');
     $that.attr ('data-n', 1).data ('l', $that.find ('>.item').length);
@@ -29,8 +30,24 @@ $(function() {
       if ($(this).hasClass ('right')) $that.attr ('data-n', n >= $that.data ('l') ? 1 : n + 1);
     });
 
-    $points.append ($('<span />').append (Array.apply (null, Array ($that.data ('l'))).map (function (_, i) { return $('<i />').attr ('data-i', i).click (function () { $that.attr ('data-n', $(this).index () + 1); }); })));
+    $points.append ($('<span />').append (Array.apply (null, Array ($that.data ('l'))).map (function (_, i) { return $('<i />').attr ('data-i', i+1).click (function () { $that.attr ('data-n', $(this).index () + 1); }); })));
 
     if ($that.data ('time')) setTimeout (function () { $a.filter ('.right').click (); }, parseInt ($that.data ('time'), 10) * 1000);
   });
+
+
+  $("#gotoTop").click(function(){
+      $("html,body").animate({
+          scrollTop:0
+      },1000);
+  });
+  $(window).scroll(function() {
+      if ( $(this).scrollTop() > 100){
+          $('#gotoTop').fadeIn("fast");
+      } else {
+          $('#gotoTop').stop().fadeOut("fast");
+      }
+  });
+
+  
 });
